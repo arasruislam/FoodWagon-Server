c93f0b962f9c4636a29d7b10ed573ea293a71aab
asru-islam <arasru01@gmail.com>
2023-05-03 02:52:22 +0600

    get the recipes and send the data

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const chef = require('./data/chef.json');
+const recipes = require("./data/recipes.json");
 const cors = require('cors');
 
 const app = express();
@@ -15,11 +16,17 @@ app.get('/chef', (req, res) => {
   res.send(chef);
 });
 
+app.get("/recipes", (req, res) => {
+  res.send(recipes);
+});
+
 app.get('/chef/:id', (req, res) => {
   const id = req.params.id;
   const foundChef = chef.find((c) => c.id === id);
   res.send(foundChef);
 })
+
+
 
 app.listen(port, () => {
   console.log(`App Listing on running port ${port}`);
