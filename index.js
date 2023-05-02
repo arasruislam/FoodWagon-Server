fc824836b0d1dc8236ea2f137daa9698c524f450
asru-islam <arasru01@gmail.com>
2023-05-03 01:10:41 +0600

    install cors, get chef.json file

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require("express");
+const chef = require('./data/chef.json');
+const cors = require('cors');
+
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("App was running");
 });
+
+app.get('/chef', (req, res) => {
+  res.send(chef);
+})
+
+// app.get('/chef/:id', (req, res) => {
+  
+// })
 
 app.listen(port, () => {
   console.log(`App Listing on running port ${port}`);
